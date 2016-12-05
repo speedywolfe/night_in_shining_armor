8709ba91b887d1f1c0307f6a7062cb30b691a2ac
Mitchell Wolfe <speedywolfe@gmail.com>
2016-12-05 14:42:45 -0700

    changed font and fixed errors

--- night_in_shining_armor/night_in_Shining_armor_Unity/Assets/Scripts/DialogueManager.cs
+++ night_in_shining_armor/night_in_Shining_armor_Unity/Assets/Scripts/DialogueManager.cs
@@ -22,8 +22,6 @@ public class DialogueManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(dialogActive && Input.GetKeyDown(KeyCode.Space)) {
-//			dBox.SetActive (false);
-//			dialogActive = false;
 
 			currentLine++;
 		}
@@ -34,8 +32,9 @@ public class DialogueManager : MonoBehaviour {
 			currentLine = 0;
 			thePlayer.canMove = true;
 		}
-
-		dText.text = dialogLines [currentLine];
+		if (dialogActive) {
+			dText.text = dialogLines [currentLine];
+		}
 	}
 
 	public void ShowBox(string dialogue) {
@@ -49,7 +48,6 @@ public class DialogueManager : MonoBehaviour {
 		dBox.SetActive (true);
 
 		thePlayer.canMove = false;
-
 	}
 
 }
