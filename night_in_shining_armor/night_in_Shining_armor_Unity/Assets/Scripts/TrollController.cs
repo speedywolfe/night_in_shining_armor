56bec18e100c3aadfdd58f6fe2792d44025391da
Mitchell Wolfe <speedywolfe@gmail.com>
2016-12-06 12:44:50 -0700

    Troll moves towards you

---
+++ night_in_shining_armor/night_in_Shining_armor_Unity/Assets/Scripts/TrollController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrollController : MonoBehaviour {
+
+//	private Vector3 moveDirection;
+
+	private GameObject Enemy;
+	private GameObject Player;
+
+	public float Speed;
+	private float Range;
+
+	// Use this for initialization
+	void Start () {
+		Enemy = GameObject.Find ("troll");
+		Player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 velocity = new Vector2((transform.position.x - Player.transform.position.x) * Speed, (transform.position.y - Player.transform.position.y) * Speed);
+		Enemy.GetComponent<Rigidbody2D>().velocity = -velocity;
+//		Enemy.transform.Translate (Vector3.left * Time.deltaTime);
+	}
+}
