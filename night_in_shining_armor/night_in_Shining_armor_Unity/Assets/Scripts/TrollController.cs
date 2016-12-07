7102e9d05c5fef08d1b531c540f75f9c02a18a13
Mitchell Wolfe <speedywolfe@gmail.com>
2016-12-06 23:19:09 -0700

    cutscene fixes

--- night_in_shining_armor/night_in_Shining_armor_Unity/Assets/Scripts/TrollController.cs
+++ night_in_shining_armor/night_in_Shining_armor_Unity/Assets/Scripts/TrollController.cs
@@ -7,20 +7,32 @@ public class TrollController : MonoBehaviour {
 
 	private GameObject Enemy;
 	private GameObject Player;
+	private DialogueHolder dHold;
+	private DialogueManager dMan;
 
 	public float Speed;
 	private float Range;
+
+	private PlayerController thePlayer;
 
 	// Use this for initialization
 	void Start () {
 		Enemy = GameObject.Find ("troll");
 		Player = GameObject.Find("Player");
+		dHold = FindObjectOfType<DialogueHolder> ();
+		dMan = FindObjectOfType<DialogueManager> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector2 velocity = new Vector2((transform.position.x - Player.transform.position.x) * Speed, (transform.position.y - Player.transform.position.y) * Speed);
 		Enemy.GetComponent<Rigidbody2D>().velocity = -velocity;
-//		Enemy.transform.Translate (Vector3.left * Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		dHold.isCutScene = false;
+		dMan.dBox.SetActive (false);
+		Application.LoadLevel ("main");
+		thePlayer.startPoint = "Main In";
 	}
 }
