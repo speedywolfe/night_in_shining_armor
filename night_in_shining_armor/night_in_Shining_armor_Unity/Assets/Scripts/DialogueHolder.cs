bbc7eef817dc4b3e0124e16ede065f8d0fe9b4c6
Mitchell Wolfe <speedywolfe@gmail.com>
2016-12-06 22:01:55 -0700

    fixed travel issues, troll animation, troll comes at you, dialogue fixes

--- night_in_shining_armor/night_in_Shining_armor_Unity/Assets/Scripts/DialogueHolder.cs
+++ night_in_shining_armor/night_in_Shining_armor_Unity/Assets/Scripts/DialogueHolder.cs
@@ -10,9 +10,12 @@ public class DialogueHolder : MonoBehaviour {
 
 	public bool isCutScene;
 
+	private PlayerController thePlayer;
+
 	// Use this for initialization
 	void Start () {
 		dMan = FindObjectOfType<DialogueManager> ();
+		thePlayer = FindObjectOfType<PlayerController> ();
 	}
 	
 	// Update is called once per frame
@@ -24,6 +27,9 @@ public class DialogueHolder : MonoBehaviour {
 		if(other.gameObject.name == "Player") {
 			if(Input.GetKeyUp(KeyCode.Space) || isCutScene) {
 //				dMan.ShowBox (dialogue);
+				if(isCutScene) {
+					thePlayer.canMove = false;
+				}
 
 				if(!dMan.dialogActive) {
 					dMan.dialogLines = dialogueLines;
