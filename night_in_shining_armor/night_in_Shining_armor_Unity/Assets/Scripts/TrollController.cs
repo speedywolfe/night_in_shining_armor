7498e6cd0222e5609ed62cbf057299669cce8ee7
Mitchell Wolfe <speedywolfe@Mitchs-MacBook-Pro.local>
2017-03-21 18:18:51 -0600

    Troll stops moving when touching player

--- night_in_shining_armor/night_in_Shining_armor_Unity/Assets/Scripts/TrollController.cs
+++ night_in_shining_armor/night_in_Shining_armor_Unity/Assets/Scripts/TrollController.cs
@@ -8,8 +8,10 @@ public class TrollController : MonoBehaviour {
 	private GameObject Enemy;
 	private GameObject Player;
 	private TrollCutScene TCut;
+	private Vector2 velocity = new Vector2(5.0f, 5.0f);
 
 	public float Speed;
+	private bool stillMoving = true;
 
 	// Use this for initialization
 	void Start () {
@@ -20,11 +22,19 @@ public class TrollController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector2 velocity = new Vector2((transform.position.x - Player.transform.position.x) * Speed, (transform.position.y - Player.transform.position.y) * Speed);
-		Enemy.GetComponent<Rigidbody2D>().velocity = -velocity;
+		if (stillMoving) {
+			velocity = new Vector2((transform.position.x - Player.transform.position.x) * Speed, (transform.position.y - Player.transform.position.y) * Speed);
+			Enemy.GetComponent<Rigidbody2D> ().velocity = -velocity;
+		}
+		else {
+			Enemy.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+			Animator anim = Enemy.GetComponent<Animator> ();
+			anim.enabled = false;
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D other) {
+		stillMoving = false;
 		TCut.trollTouched ();
 		TCut.startCutScene = true;
 	}
