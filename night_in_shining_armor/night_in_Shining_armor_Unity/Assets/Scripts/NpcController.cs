27615be3a5b1c7fb9c4313475bafd6f2043c9d29
Mitchell Wolfe <speedywolfe@Mitchs-MacBook-Pro.local>
2017-03-23 10:45:24 -0600

    door blocks academy exit, has bugs though

---
+++ night_in_shining_armor/night_in_Shining_armor_Unity/Assets/Scripts/NpcController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcController : MonoBehaviour {
+
+	private GameObject door;
+	private bool talkedToGuy;
+
+	void Start() {
+		door = GameObject.Find ("outer_door");
+	}
+
+	void Update() {
+		if(talkedToGuy) {
+			door.SetActive(false);
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		if(other.gameObject.name == "Player") {
+			if(Input.GetKeyUp(KeyCode.Space)) {
+				talkedToGuy = true;
+			}
+		}
+	}
+}
