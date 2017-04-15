9507f6f252da0871403bddb69304a562eb3c17f3
Mitchell Wolfe <speedywolfe@Mitchs-MacBook-Pro.local>
2017-04-15 13:32:57 -0600

    2nd troll fight works

---
+++ night_in_shining_armor/night_in_Shining_armor_Unity/Assets/Scripts/TrollController2.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrollController2 : MonoBehaviour {
+
+//	private Vector3 moveDirection;
+
+	private GameObject Enemy;
+	private GameObject Player;
+	private Vector2 velocity = new Vector2(5.0f, 5.0f);
+
+	public float Speed;
+	private bool stillMoving = true;
+
+	// Use this for initialization
+	void Start () {
+		Enemy = GameObject.Find ("troll2");
+		Player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (stillMoving) {
+			velocity = new Vector2((transform.position.x - Player.transform.position.x) * Speed, (transform.position.y - Player.transform.position.y) * Speed);
+			Enemy.GetComponent<Rigidbody2D> ().velocity = -velocity;
+		}
+		else {
+			Enemy.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+			Animator anim = Enemy.GetComponent<Animator> ();
+			anim.enabled = false;
+		}
+	}
+}
