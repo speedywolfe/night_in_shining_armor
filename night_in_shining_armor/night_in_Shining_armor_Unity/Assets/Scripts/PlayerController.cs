f14360519ab06a0b8981e28b7d9a440f5f0e20dc
Mitchell Wolfe <speedywolfe@Mitchs-MacBook-Pro.local>
2017-04-15 12:52:58 -0600

    player speed changes with confidence

--- night_in_shining_armor/night_in_Shining_armor_Unity/Assets/Scripts/PlayerController.cs
+++ night_in_shining_armor/night_in_Shining_armor_Unity/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour {
 
 	private Animator anim;
 	private Rigidbody2D myRigidbody;
+	private ConfidenceManager cMan;
 
 	private bool playerMoving;
 
@@ -26,6 +27,7 @@ public class PlayerController : MonoBehaviour {
 	void Start () {
 		anim = GetComponent<Animator> ();
 		myRigidbody = GetComponent<Rigidbody2D> ();
+		cMan = GetComponent<ConfidenceManager> ();
 
 		if (!playerExists) {
 			playerExists = true;
@@ -47,17 +49,18 @@ public class PlayerController : MonoBehaviour {
 		}
 
 		if (!attacking) {
-
+			float conf = (float)cMan.playerCurrentConfidence;
+			float currentSpeed = moveSpeed - (2*((100-conf)/100));
 			if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw ("Horizontal") < -0.5f) {
 				//transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f ));
-				myRigidbody.velocity = new Vector2 (Input.GetAxisRaw ("Horizontal") * moveSpeed, myRigidbody.velocity.y);
+				myRigidbody.velocity = new Vector2 (Input.GetAxisRaw ("Horizontal") * currentSpeed, myRigidbody.velocity.y);
 				playerMoving = true;
 				lastMove = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0f);
 
 			}
 			if (Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw ("Vertical") < -0.5f) {
 				//transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, Input.GetAxisRaw ("Vertical") * moveSpeed);
+				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, Input.GetAxisRaw ("Vertical") * currentSpeed);
 				playerMoving = true;
 				lastMove = new Vector2 (0f, Input.GetAxisRaw ("Vertical"));
 
